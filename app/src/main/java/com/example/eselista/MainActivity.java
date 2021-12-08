d605958d8842af0bc2898b4a57cae43e2c7bb754
leonardo <leonardoroscini.03@gmail.com>
2021-12-08 21:57:01 +0100

    iniziale

---
+++ app/src/main/java/com/example/eselista/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.eselista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.b1);
+    }
+    public void play(View r){
+        Intent i;
+    }
+}
